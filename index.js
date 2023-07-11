7f1ca9bd9dab351db1aae8238c407b04d22658f7
adriennemadarang <adriennemadarang@icloud.com>
2023-07-11 01:14:55 -0700

    added prompts

--- index.js
+++ index.js
@@ -5,6 +5,60 @@ const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your project name?', 
+        name: 'title',
+    }, 
+    
+    {
+        type: 'input', 
+        message: 'Description of project', 
+        name: 'description',
+    },
+
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed', 
+        name: 'usage',
+    },
+
+    {
+        type: 'list',
+        message: 'Choose license for your project.', 
+        name: 'license',
+        choices: [
+            {value: 'Apache'},
+            {value: 'GNU'},
+            {value: 'MIT License'},
+            {value: 'BSD 3'},
+            {value: 'Boost'},
+            {value: 'Creative'},
+            {value: 'Eclipse'},
+            {value: 'GNU'},
+            {value: 'Mozilla'},
+            {value: 'None'},
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Explain how users can contribute to your project (optional).', 
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your e-mail address:', 
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username:', 
+        name: 'username',
+    },
+
+])
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
